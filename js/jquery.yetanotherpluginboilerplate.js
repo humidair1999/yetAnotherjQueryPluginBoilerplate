9005c12dd7deb4469f2d7ad03f144232eef2fb94
J. Ky Marsh <jkymarsh@gmail.com>
2013-05-16 00:26:36 -0500

    Wow, this .data thing is really going to piss me off

--- js/jquery.yetanotherpluginboilerplate.js
+++ js/jquery.yetanotherpluginboilerplate.js
@@ -8,32 +8,23 @@
 // the semi-colon before function invocation is a safety net against concatenated
 // scripts and/or other plugins which may not be closed properly.
 ;(function ($, window, document, undefined) {
-        // namespace will generally be the name of author/company/organization
+    // namespace will generally be the name of author/company/organization
     var pluginNamespace = "JKYMARSH",
         // name/version of the plugin (come on, bro)
         pluginName = "testPlugin",
         pluginVersion = "0.1.0";
 
-    console.log(pluginNamespace);
-    console.log($[pluginNamespace]);
-
     if (!$[pluginNamespace]) {
         $[pluginNamespace] = {};
     }
 
-    console.log($.JKYMARSH);
-    console.log($[pluginNamespace]);
-
-    $[pluginNamespace][pluginName] = function(element, options, pluginName) {
+    $[pluginNamespace][pluginName] = function(element, options) {
         // To avoid scope issues, use 'base' instead of 'this'
         // to reference this class from internal events and functions.
-        var base = this;
-
-        // Access to jQuery and DOM versions of element
-        base.$el = $(element);
-
-        // Add a reverse reference to the DOM object
-        base.$el.data("myNamespace.myPluginName", base);
+        var base = {
+            $element: $(element),
+            options: options
+        };
 
         var _callbackSupport = function(callback) {
             //Checks to make sure the parameter passed in is a function
@@ -47,10 +38,9 @@
                 that called the plugin(wrapped in a jQuery object) is the only 
                 parameter passed back to the callback
                 */
-                callback.call(self.$element.data(dataName), self.$element);
+                callback.call(base.$element.data(pluginName), base.$element);
             }
  
-            //Maintains chainability
             return this;
         };
 
@@ -60,7 +50,6 @@
             //Provides callback function support
             _callbackSupport(callback);
                
-            //Maintains chainability
             return this;
         };
 
@@ -69,65 +58,48 @@
         }
     };
 
+    // set default options for the plugin
     $[pluginNamespace][pluginName].defaultOptions = {
         myDefaultValue: ""
     };
 
     if (!$.fn[pluginNamespace + "_" + pluginName]) {
-        // $.fn[pluginNamespace + "_" + pluginName] = function(myFunctionParam, options) {
-        //     return this.each(function () {
-        //         new $[pluginNamespace][pluginName](this, myFunctionParam, options);
-        //     });
-        // };
-
         $.fn[pluginNamespace + "_" + pluginName] = function(options) {
-         
             // maintains chainability for all calling elements
             return this.each(function () {
-                var element = $(this),
+                var $element = $(this),
                     plugin,
-                    dataName = pluginName,
                     obj = {};
          
-                if ($.data(element[0], dataName)) { 
-                    return;
+                if (!$.data($element[0], pluginName)) { 
+                    options = $.extend({}, $[pluginNamespace][pluginName].defaultOptions, options);
+             
+                    $.data($element[0], pluginName, new $[pluginNamespace][pluginName](this, options).create());
+     
+                    obj[pluginName] = function(elements) {
+                        // TODO: why in the fuck can I not consistently use .data methods?
+                        return $(elements).data(pluginName) !== undefined;
+                    };
+
+                    // adds custom jquery pseudo selector
+                    //  search for all DOM elements with plugin instances using:
+                    //      $(":pluginName").each(function() { });
+                    $.extend($.expr[":"], obj);
                 }
-         
-                options = $.extend({}, $[pluginNamespace][pluginName].defaultOptions, options);
-             
-                // Instantiates a new `Plugin` object and creates the plugin
-                plugin = new $[pluginNamespace][pluginName](this, options).create();
-             
-                /*
-                Stores the new `Plugin` object in the calling element's 
-                jQuery `data` method
-                */
-                $.data(element[0], dataName, plugin);
+                else {
+                    console.warn("plugin with same name (" +
+                        pluginName +
+                        ") already exists on element: ",
+                        $element);
+                }
+            });
  
-                /*
-                Uses the name of the plugin to create a dynamic property 
-                of an empty object literal
-                */
-                obj[pluginName] = function(elem) {
-                    /*
-                    Returns all DOM elements that have jQuery `data()`
-                    created by the plugin
-                    */
-                    return $(elem).data(dataName) !== undefined;
-                };
- 
-                //Adds custom jQuery pseudo selectors
-                $.extend($.expr[":"], obj); 
-                //end extending jQuery pseudo selectors
-         
-            }); //end return statement
- 
-        };  //end plugin
+        };
     }
     else {
-        console.warn("plugin with same name (" +
+        console.warn("jQuery plugin function with same name (" +
             [pluginNamespace + "_" + pluginName] +
-            ") already exists!");
+            ") has already been loaded!");
     }
 
 })(jQuery, window, document);
