e3815e292625498250dcc05abb6c506feddc07e1
J. Ky Marsh <jkymarsh@gmail.com>
2013-05-16 23:30:29 -0500

    Add getter/setter, add destroy() method, add option to pass in create() callback, minor cleanup

--- js/jquery.yetanotherpluginboilerplate.js
+++ js/jquery.yetanotherpluginboilerplate.js
@@ -8,82 +8,90 @@
 // the semi-colon before function invocation is a safety net against concatenated
 // scripts and/or other plugins which may not be closed properly.
 ;(function ($, window, document, undefined) {
-    // namespace will generally be the name of author/company/organization
-    var pluginNamespace = "JKYMARSH",
-        // name/version of the plugin (come on, bro)
-        pluginName = "testPlugin",
+    // name/version of the plugin (come on, bro)
+    var pluginName = "testPlugin",
         pluginVersion = "0.1.0",
         // empty object for containing our new jquery selector ($(":pluginName")) key/value
-        selectorObj = {};
+        selectorObj = {},
+        // default options for the plugin
+        defaultOptions = {
+            someDefaultValue: null,
+            someOtherDefaultValue: "a string"
+        },
+        // plugin constructor using module design pattern
+        Plugin = function(element, options) {
+            // To avoid scope issues, use 'base' instead of 'this'
+            // to reference this class from internal events and functions.
+            var base = {
+                $element: $(element),
+                options: options
+            };
 
-    if (!$[pluginNamespace]) {
-        $[pluginNamespace] = {};
-    }
+            console.log(options);
 
-    $[pluginNamespace][pluginName] = function(element, options) {
-        // To avoid scope issues, use 'base' instead of 'this'
-        // to reference this class from internal events and functions.
-        var base = {
-            $element: $(element),
-            options: options
-        };
+            var _fireCallback = function(callback) {
+                //Checks to make sure the parameter passed in is a function
+                if($.isFunction(callback)) {
+                    /*
+                    Calls the method passed in as a parameter and sets the context to
+                    the `Plugin` object, which is stored in the jQuery `data()`
+                    method.  This allows for the `this` context to reference the
+                    Plugin API Methods in the callback function. The original element
+                    that called the plugin(wrapped in a jQuery object) is the only
+                    parameter passed back to the callback
+                    */
+                    callback.call(base.$element.data(pluginName), base.$element);
+                }
+     
+                return this;
+            };
 
-        console.log(options);
+            var someMethod = function() {
+                console.log("called someMethod");
 
-        var _callbackSupport = function(callback) {
-            //Checks to make sure the parameter passed in is a function
-            if($.isFunction(callback)) {
- 
-                /*
-                Calls the method passed in as a parameter and sets the context to 
-                the `Plugin` object, which is stored in the jQuery `data()`
-                method.  This allows for the `this` context to reference the 
-                Plugin API Methods in the callback function. The original element 
-                that called the plugin(wrapped in a jQuery object) is the only 
-                parameter passed back to the callback
-                */
-                callback.call(base.$element.data(pluginName), base.$element);
+                return this;
+            };
+
+            return {
+                create: function(callback) {
+                    // Put your initialization code here
+
+                    //Provides callback function support
+                    _fireCallback(callback);
+                       
+                    return this;
+                },
+                destroy: function(callback) {
+                    base.$element.removeData(pluginName);
+
+                    _fireCallback(callback);
+                       
+                    return this;
+                },
+                option: function(key, value) {
+                    if (value) {
+                        base.options[key] = value;
+                    }
+                    else {
+                        return base.options[key];
+                    }
+
+                    console.log(base.options);
+                },
+                someMethod: someMethod
             }
- 
-            return this;
         };
 
-        var create = function(callback) {
-            // Put your initialization code here
-
-            //Provides callback function support
-            _callbackSupport(callback);
-               
-            return this;
-        };
-
-        var someMethod = function() {
-            console.log("called someMethod");
-
-            return this;
-        };
-
-        return {
-            create: create,
-            someMethod: someMethod
-        }
-    };
-
-    // set default options for the plugin
-    $[pluginNamespace][pluginName].defaultOptions = {
-        someDefaultValue: ""
-    };
-
-    if (!$.fn[pluginNamespace + "_" + pluginName]) {
-        $.fn[pluginNamespace + "_" + pluginName] = function(options) {
+    if (!$.fn[pluginName]) {
+        $.fn[pluginName] = function(options, callback) {
             // maintains chainability for all calling elements
             return this.each(function () {
                 var $element = $(this);
          
                 if (!$.data($element[0], pluginName)) { 
-                    options = $.extend({}, $[pluginNamespace][pluginName].defaultOptions, options);
+                    options = $.extend({}, defaultOptions, options);
              
-                    $.data($element[0], pluginName, new $[pluginNamespace][pluginName](this, options).create());
+                    $.data($element[0], pluginName, new Plugin(this, options).create(callback));
                 }
                 else {
                     console.warn("plugin with same name (" +
@@ -95,8 +103,8 @@
         };
     }
     else {
-        console.warn("jQuery plugin function with same name (" +
-            [pluginNamespace + "_" + pluginName] +
+        console.warn("jQuery plugin function with same name ($.fn." +
+            pluginName +
             ") has already been loaded!");
     }
 
